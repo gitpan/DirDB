2726714bdb929490952912690a42ffab5f90e04f
David Nicol <davidnicol@gmail.com>
2002-06-25 04:07:30 +0000

    Import of DAVIDNICO/DirDB-0.04 from CPAN.
    gitpan-cpan-distribution: DirDB
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         DAVIDNICO/DirDB-0.04.tar.gz
    gitpan-cpan-author:       DAVIDNICO
    gitpan-cpan-maturity:     released

--- DirDB.pm
+++ DirDB.pm
@@ -5,7 +5,10 @@ use strict;
 use warnings;
 use Carp;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
+
+# @ISA = qw(Tie::Array);
+
 
 
 # Preloaded methods go here.
@@ -28,13 +31,32 @@ our $VERSION = '0.03';
 sub TIEHASH {
 	my $self = shift;
 	my $rootpath = shift or croak "we need a rootpath";
-	'/' eq substr($rootpath , -1) or $rootpath .= '/';
+	$rootpath =~ s#/$##; # lose trailing slash for MacOS
 	-d $rootpath or
 	   mkdir $rootpath, 0777 or
 	     croak "could not create dir $rootpath: $!";
 
 
+	$rootpath .= '/';
 	bless \$rootpath, $self;
+};
+
+sub TIEARRAY {
+
+	confess "DirDB does not support arrays, use DirDB::Array";
+
+#	my $self = shift;
+#	my $rootpath = shift or croak "we need a rootpath";
+#	$rootpath =~ s#/$##; # lose trailing slash for MacOS
+#	-d $rootpath or
+#	   mkdir $rootpath, 0777 or
+#	     croak "could not create dir $rootpath: $!";
+#
+#
+#	$rootpath .= '/';
+#	my $Object = bless \$rootpath, $self;
+#	$Object->STOREMETA('ARRAY', 0);
+#	return $Object;
 };
 
 
@@ -42,18 +64,30 @@ sub TIEHASH {
 sub EXISTS {
 	my $rootpath = ${+shift};
 	my $key = shift;
+	$key =~ s/^ /  /; #escape leading space into two spaces
+	# defined (my $key = shift) or return undef;
+	$key eq '' and $key = ' EMPTY';
 	-e "$rootpath$key";
 };
 
 sub FETCH {
 	my $ref = shift;
 	defined (my $rootpath = $$ref) or croak "undefined rootpath";
-	defined (my $key = shift) or return undef;
+	my $key = shift;
+	$key =~ s/^ /  /; #escape leading space into two spaces
+	# defined (my $key = shift) or return undef;
+	$key eq '' and $key = ' EMPTY';
 	-e "$rootpath$key" or return undef;
 	if(-d "$rootpath$key"){
 
+		#if -e ("$rootpath ARRAY"{
+		#	tie my @array, ref($ref),"$rootpath$key";
+ 		#	return \@array;
+		#};
+ 
 		tie my %hash, ref($ref),"$rootpath$key";
  		return \%hash;
+	
 
 	};
 
@@ -72,8 +106,37 @@ sub STORE {
 
 	
 	my $key = shift;
+	$key =~ s/^ /  /; #escape leading space into two spaces
+	$key eq '' and $key = ' EMPTY';
 	my $value = shift;
 	ref $value and croak "This hash does not support storing references";
+	open FSDBFH,">$rootpath${$}TEMP$key" or croak $!;
+	print FSDBFH $value;
+	close FSDBFH;
+	rename "$rootpath${$}TEMP$key", "$rootpath$key" or croak $!;
+};
+
+sub FETCHMETA {
+	my $ref = shift;
+	defined (my $rootpath = $$ref) or croak "undefined rootpath";
+	my $key = ' '.shift;
+	-e "$rootpath$key" or return undef;
+	if(-d "$rootpath$key"){
+
+		confess "Complex metadata not supported in DirDB version $VERSION";	
+
+	};
+
+	open FSDBFH, "<$rootpath$key"
+	   or croak "cannot open $rootpath$key: $!";
+	join '', (<FSDBFH>);
+};
+
+sub STOREMETA {
+	my $rootpath = ${+shift}; # RTFM! :)
+	my $key = ' '.shift;
+	my $value = shift;
+	ref $value and croak "DirDB does not support storing references in metadata at version $VERSION";
 	open FSDBFH,">$rootpath${$}TEMP$key" or croak $!;
 	print FSDBFH $value;
 	close FSDBFH;
@@ -83,6 +146,8 @@ sub STORE {
 sub DELETE {
 	my $rootpath = ${+shift};
 	my $key = shift;
+	$key =~ s/^ /  /; #escape leading space into two spaces
+	$key eq '' and $key = ' EMPTY';
 	if(-d "$rootpath$key"){
 		rmdir "$rootpath$key"
 		   or croak "could not delete directory $rootpath$key: $!";
@@ -106,7 +171,7 @@ sub CLEAR{
 		$entity = join('',$path,$entity);
 		if(-d $entity){
 		 rmdir "$entity"
-		   or croak "could not delete directory $entity: $!";
+		   or croak "could not delete (sub-container?) directory $entity: $!";
 		};
 		unlink $entity;
 	};
@@ -132,6 +197,21 @@ sub CLEAR{
 	#print "next key in path <$$ref> will be shifted from <@{$IteratorListings{$ref}}>\n";
 	@{$IteratorListings{$ref}} or return undef;
 	my $key = shift @{$IteratorListings{$ref}};
+	if ($key =~ s/^ //){
+		if ($key = m/^ /){
+			# we have unescaped a leading space.
+		}elsif ($key eq 'EMPTY'){
+			$key = ''
+		#}elsif($key eq 'REF'){
+		# 	return $ref->NEXTKEY();	# next
+		#}elsif($key =~ m/^ARRAY){
+		# 	return $ref->NEXTKEY();	# next
+		}else{
+			# per-container metadata does not
+			# appear in iterations through data.
+			return $ref->NEXTKEY();	# next
+		}
+	};
 	wantarray or return $key;
 	return @{[$key, $ref->FETCH($key)]};
    };
@@ -163,7 +243,19 @@ DirDB is a package that lets you access a directory
 as a hash. The final directory will be created, but not
 the whole path to it.
 
-DirDB croaks on attempts to store references.
+The empty string, used as a key, will be translated into
+' EMPTY' for purposes of storage and retrieval.  File names
+beginning with a space are reserved for metadata for subclasses,
+such as object type or array size or whatever.  Key names beginning
+with a space get an additional space prepended to the name
+for purposes of naming the file to store that value.
+
+DirDB croaks on attempts to store references. There are
+hooks in place as of version 0.04 to do something more sensible, 
+like recursively blatting out the whole object tree being
+referred to, but to prevent DirDB being involved in the
+possible endless loop problems, implementing such behavior is
+left for subclassed to do.
 
 DirDB will croak if it can't open an existing file system
 entity, so wrap your fetches in eval blocks if there are
@@ -185,6 +277,12 @@ If anyone cares to benchmark DirDB on ReiserFS against
 Berkeley DB for databases of verious sizes, please send me
 the results and I will include them here.
 
+STOREMETA and FETCHMETA methods are provided for subclasses
+who which to store and fetch metadata (such as array size)
+which will not appear in the data returned by NEXTKEY and which
+cannot be accessed directly through STORE or FETCH.  DirDB::Array
+will store its size and so on using these methods.
+
 
 =head2 EXPORT
 
@@ -201,6 +299,12 @@ QA provided by members of Kansas City Perl Mongers, including
 Andrew Moore and Craig S. Cottingham.
 
 =head1 LICENSE
+
+GPL
+
+=head1 SEE ALSO
+
+better read <l perltie> before trying to extend this
 
 GPL
 
